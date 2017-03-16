8bf114dde04ff529cc63f516d33d19274b921467
Art Dahm <adahm@pearlholding.com>
2017-03-16 09:46:39 -0400

    Implement router for favorites/history

---
+++ src/components/Favorites.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    These are Favorites
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'favorites',
+    props: {
+    },
+    data () {
+      return {
+      }
+    }
+  })
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,27 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap/js/modal');
 
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import RollHistory from './components/RollHistory.vue'
+import Favorites from './components/Favorites.vue'
+
+const routes = [
+  { path: '/history', component: RollHistory },
+  { path: '/favorites', component: Favorites }
+]
+
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
+})
+
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+}).$mount('#app')
